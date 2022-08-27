dd6a5f827fd34628fbc73f74876bc88626f45124
Austin Gae <austingae@gmail.com>
2022-08-27 12:18:37 -0400

    Display data from the database onto the website page using PHP and SQL

--- index.php
+++ index.php
@@ -1,43 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Vivi and Calla</title>
+<!-- Connection to the Database -->
+<?php 
+  require 'includes/database-handler.php';
+?>
 
-  <link rel='stylesheet' type='text/css' href='styles.css' />
-</head>
-<body>
-  <?php
-  $dayOfWeek = date('w');
-  
-  switch($dayOfWeek) {
-    case 1: 
-      echo 'It&apos;s Monday';
-      break;
-    case 2: 
-        echo 'It&apos;s Tuesday';
-        break;
-    case 3: 
-        echo 'It&apos;s Wednesday';
-        break;
-    case 4: 
-        echo 'It&apos;s Thursday';
-        break;
-    case 5: 
-        echo '<p class=day>It&apos;s Friday</p>';
-        break;
-    case 6: 
-        echo 'It&apos;s Saturday';
-        break;
-    case 0: 
-        echo 'It&apos;s Sunday';
-        break;
+<!-- Navigation Bar-->
+<?php
+  require 'header.php';
+?> 
+
+<?php 
+  $sql = 'SELECT * FROM users;'; //SQL SELECT STATEMENT
+  $result = mysqli_query($conn, $sql); //Use the $sql, aka the SQL SELECT STATEMENT, in $conn, the database.
+
+  $resultCheck = mysqli_num_rows($result);
+
+  if ($resultCheck > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      echo $row['user_first'];
+      echo '<br>';
+    }
   }
-  ?>
+?>
 </body>
 </html>
-
-
 
