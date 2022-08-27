ebaa28ad7846a1d72afe5afdc73df5301143a943
Austin Gae <austingae@gmail.com>
2022-08-27 14:54:00 -0400

    second way to insert data from website and transfer it to the database using PHP and SQL

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   require 'header.php';
 ?> 
 
-<form method='POST'>
+<form action ='includes/signup.php' method='POST'>
   <input type='text' name='firstName' placeholder='your first name' />
   <input type='text' name='lastName' placeholder='your last name' />
   <input type='text' name='email' placeholder='your email' />
@@ -17,16 +17,6 @@
   <button type='submit' name='submit'>Sign Up</button>
 </form>
 
-<?php 
-  $firstName = $_POST['firstName'];
-  $lastName = $_POST['lastName'];
-  $email = $_POST['email'];
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-
-  $sqlStatement = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$firstName', '$lastName', '$email', '$username', '$password');";
-  $result = mysqli_query($conn, $sqlStatement);
-?>
 </body>
 </html>
 
